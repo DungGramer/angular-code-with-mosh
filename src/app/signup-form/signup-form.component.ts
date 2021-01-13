c2766566e65ec4a355dde829f48b0f3a7b85cd28
DungGramer <dung.dev.gramer@gmail.com>
2021-01-13 13:41:21 +0700

    nested formGroup

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -8,17 +8,19 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class SignupFormComponent {
   form = new FormGroup({
-    username: new FormControl(
-      '',
-      Validators.required,
-      // Validators.minLength(3),
-      // UsernameValidators.cannotContainSpace,
-      UsernameValidators.shouldBeUnique
-    ),
-    password: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-    ]),
+    account: new FormGroup({
+      username: new FormControl(
+        '',
+        Validators.required,
+        // Validators.minLength(3),
+        // UsernameValidators.cannotContainSpace,
+        UsernameValidators.shouldBeUnique
+      ),
+      password: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+      ]),
+    }),
   });
 
   // tslint:disable-next-line:typedef
@@ -29,6 +31,6 @@ export class SignupFormComponent {
   }
 
   get username(): any {
-    return this.form.get('username');
+    return this.form.get('account.username');
   }
 }
